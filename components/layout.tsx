d71c55f53c14d333c0830e1342e50999a58527df
mbrunetto <matteo.brunetto@lastminute.com>
2023-03-03 11:19:40 +0100

    migrate to typescript

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,11 +7,18 @@ import Link from 'next/link';
 const name = 'Matteo';
 export const siteTitle = 'Next.js Sample Website';
 
-export default function Layout({ children, home }) {
+export default function Layout(
+    {
+      children,
+      home
+    }: {
+      children: React.ReactNode
+      home?: boolean
+    }) {
   return (
       <div className={styles.container}>
         <Head>
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="/favicon.ico"/>
           <meta
               name="description"
               content="Learn how to build a personal website using Next.js"
@@ -19,11 +26,11 @@ export default function Layout({ children, home }) {
           <meta
               property="og:image"
               content={`https://og-image.vercel.app/${encodeURI(
-                  siteTitle,
-              )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+                  siteTitle
+              )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
           />
-          <meta name="og:title" content={siteTitle} />
-          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="og:title" content={siteTitle}/>
+          <meta name="twitter:card" content="summary_large_image"/>
         </Head>
         <header className={styles.header}>
           {home ? (
@@ -34,7 +41,7 @@ export default function Layout({ children, home }) {
                     className={utilStyles.borderCircle}
                     height={144}
                     width={144}
-                    alt=""
+                    alt={name}
                 />
                 <h1 className={utilStyles.heading2Xl}>{name}</h1>
               </>
@@ -47,7 +54,7 @@ export default function Layout({ children, home }) {
                       className={utilStyles.borderCircle}
                       height={108}
                       width={108}
-                      alt=""
+                      alt={name}
                   />
                 </Link>
                 <h2 className={utilStyles.headingLg}>
@@ -65,5 +72,5 @@ export default function Layout({ children, home }) {
             </div>
         )}
       </div>
-  );
+  )
 }
